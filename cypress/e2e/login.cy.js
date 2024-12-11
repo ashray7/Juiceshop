b59e22c46513bb467a84ead7793ddf34d4b9107b
Ashray Baral <ashraybaral77@gmail.com>
2024-12-11 09:24:30 +0545

    to ensure everything upto date

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -39,7 +39,7 @@ describe('Login Test Suite', () => {
     beforeEach(() => {
         cy.visit(url);
 
-        cy.wait(3000);
+        cy.wait(1000);
 
         // Close the welcome pop-up
         cy.get(selectors.closeDialog).should('be.visible').click();
